4ac2589a5aa73e281774b791c53189347b177260
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2024-01-08 12:12:30 +0700

    update cv

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <footer id="footer" className='text-center'  data-aos="fade-in">
             <a className='btn btn-danger' target='_blank' href="https://www.linkedin.com/in/luan-vo-44b123237">Linkedin</a>
+            <a className='btn btn-primary ms-3' target='_blank' href="https://drive.google.com/file/d/185jkMZfE3j9JLC-HqycP-LhIM_rf_flw/view?usp=drive_link">Tải CV</a>
         </footer>
     )
 }
